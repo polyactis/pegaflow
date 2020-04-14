72122eb31a4d1c186d6e78c7ca8b6b194f91eef6
Yu Huang@puffer <polyactis@gmail.com>
2020-04-14 20:55:43 +0800

    bump to 0.0.5 after bugfixes

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def find_package_data(dirname):
 
 setup_args = dict(
     name="Pegaflow",
-    version="0.0.4",
+    version="0.0.5",
     author="Yu S. Huang",
     author_email="polyactis@gmail.com",
     description="Pegasus DAX Python API with a helper class",
