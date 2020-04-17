f853da355ae1684eae6dbe5d77c4b36d3f1cf074
Yu Huang@cichlet <polyactis@gmail.com>
2020-04-17 18:06:30 +0800

    v 0.0.10

--- pegaflow/__init__.py
+++ pegaflow/__init__.py
@@ -9,7 +9,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-version='0.0.9'
+version='0.0.10'
 import logging
 import os
 import sys
