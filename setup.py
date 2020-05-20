965514634ea2f0b049f0b17e77a1dc2a794614b0
Yu Huang@neon <polyactis@gmail.com>
2020-05-20 12:17:44 +0800

    v 0.1.5

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(dirname):
 
 setup_args = dict(
     name="Pegaflow",
-    version="0.1.4",
+    version="0.1.5",
     author="Yu S. Huang",
     author_email="polyactis@gmail.com",
     description="Pegasus DAX Python API with a helper class",
