979d3b8985c0115b60d244534dc37ab42428c388
Yu Huang@neon <polyactis@gmail.com>
2020-06-08 15:55:31 +0800

    v 0.2.5

--- pegaflow/__init__.py
+++ pegaflow/__init__.py
@@ -15,7 +15,7 @@ import sys
 from . DAX3 import Executable, File, PFN, Profile, Namespace, Link
 from . DAX3 import Job, Dependency
 
-version = '0.2.4'
+version = '0.2.5'
 namespace = "pegasus"
 pegasus_version = "1.0"
 architecture = "x86_64"
