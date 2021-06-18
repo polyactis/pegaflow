bc65e24d0760672c70109885e86df64b3cb8b32f
Yu Huang@superman <polyactis@gmail.com>
2021-06-18 16:32:12 +0800

    version 5.0.3

--- pegaflow/__init__.py
+++ pegaflow/__init__.py
@@ -7,7 +7,7 @@ from .api import Directory, File, FileServer, Job, Operation, \
 from .api import Arch, OS
 from .api import Workflow as PegaWorkflow
 
-version = '5.0.2'
+version = '5.0.3'
 namespace = "pegasus"
 pegasus_version = "1.0"
 
